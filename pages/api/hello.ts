d70d22c4fc8ad089a59bc18c4fb003cd6e3f0255
Harold <harold@pwrdrvr.com>
2021-02-07 15:24:48 -0500

    Update hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   res.statusCode = 200;
   res.json({ name: 'John Doe' });
-};
+}
+
+// export default function handler(req, res) {
+//   res.status(200).json({ text: 'Hello' })
+// }
