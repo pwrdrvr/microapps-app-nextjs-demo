add3d85ee4e968992aad4dab07c44eadf5d8f923
Harold <harold@pwrdrvr.com>
2021-02-26 20:39:42 -0500

    Grant access to S3

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -5,5 +5,11 @@ import { ReposStack } from '../lib/repos';
 import { SvcsStack } from '../lib/svcs';
 
 const app = new cdk.App();
+
+// Add a tag indicating this app can be managed by the
+// MicroApp Deployer Lambda function
+cdk.Tags.of(app).add('microapp-managed', 'true');
+cdk.Tags.of(app).add('microapp-name', 'nexjs-demo');
+
 const reposStack = new ReposStack(app, 'AppNextJsDemoRepos');
 new SvcsStack(app, 'AppNextJsDemoSvcs', { reposExports: reposStack });
--- cdk/lib/svcs.ts
+++ cdk/lib/svcs.ts
@@ -1,7 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
-import * as ecr from '@aws-cdk/aws-ecr';
 import * as iam from '@aws-cdk/aws-iam';
+import * as s3 from '@aws-cdk/aws-s3';
 import { IReposExports } from './repos';
 
 export interface ISvcsProps extends cdk.StackProps {
@@ -11,9 +11,6 @@ export interface ISvcsProps extends cdk.StackProps {
 export class SvcsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props: ISvcsProps) {
     super(scope, id, props);
-    // Add a tag indicating this app can be managed by the
-    // MicroApp Deployer Lambda function
-    cdk.Tags.of(this).add('microapp-managed', 'true');
 
     //
     // Lambda Function
@@ -26,5 +23,12 @@ export class SvcsStack extends cdk.Stack {
         NODE_ENV: 'production',
       },
     });
+
+    //
+    // S3 bucket for deployed apps
+    // Next.js apps need read/write access to their directory
+    //
+    const bucket = s3.Bucket.fromBucketName(this, 'pwrdrvr-apps-bucket', 'pwrdrvr-apps');
+    bucket.grantReadWrite(svc);
   }
 }
