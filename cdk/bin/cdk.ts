c06d05a93e3e290f4f7df4a4ad712627f75cd0a5
Harold <harold@pwrdrvr.com>
2021-02-26 20:44:30 -0500

    Fix app name typo

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -9,7 +9,7 @@ const app = new cdk.App();
 // Add a tag indicating this app can be managed by the
 // MicroApp Deployer Lambda function
 cdk.Tags.of(app).add('microapp-managed', 'true');
-cdk.Tags.of(app).add('microapp-name', 'nexjs-demo');
+cdk.Tags.of(app).add('microapp-name', 'nextjs-demo');
 
 const reposStack = new ReposStack(app, 'AppNextJsDemoRepos');
 new SvcsStack(app, 'AppNextJsDemoSvcs', { reposExports: reposStack });
