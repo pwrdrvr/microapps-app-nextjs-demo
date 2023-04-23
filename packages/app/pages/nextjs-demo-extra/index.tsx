1e0e5b09dc858c3fa00c9248fddcf2f64acac38b
harold <harold@pwrdrvr.com>
2023-04-23 10:02:12 -0400

    Add an extra top-level route

---
+++ packages/app/pages/nextjs-demo-extra/index.tsx
@@ -0,0 +1,75 @@
+import Head from 'next/head';
+import React from 'react';
+import { useTranslation } from 'next-i18next';
+import Link from 'next/link';
+import Layout, { siteTitle } from '../../components/layout';
+import utilStyles from '../../styles/utils.module.css';
+import { getSortedPostsData } from '../../lib/posts';
+import Date from '../../components/date';
+import { getServerTranslations } from '../../lib/getServerTranslations';
+
+//
+// getStaticProps runs server-side and is not included in the client-side
+// code bundle at all.
+//
+// Dev: Runs on every page load
+// Prod: Runs on build
+//
+// export async function getStaticProps(): Promise<
+//   GetStaticPropsResult<{ allPostsData: ReturnType<typeof getSortedPostsData> }>
+// > {
+//   const allPostsData = getSortedPostsData();
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+
+//
+// getServerSideProps - Runs on every page load.
+// Used for server-side rendering with data accessed from server-side.
+// TTFB will be delayed by duration of server-side calls for data.
+//
+// SWR - Next.js provided client side data reloader... suggested to use.
+//
+
+export async function getServerSideProps(context) {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+      ...(await getServerTranslations(context.locale, ['common'])),
+    },
+  };
+}
+
+export default function Home({ allPostsData }): JSX.Element {
+  const { t, i18n } = useTranslation('common');
+  return (
+    <Layout home locale={i18n.language}>
+      <Head>
+        <title>PwrDrvr - Extra Route Demo</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>{t('introduction')}</p>
+        <p>This is a demo of an extra top-level page route in a single MicroApp.</p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/nextjs-demo/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
